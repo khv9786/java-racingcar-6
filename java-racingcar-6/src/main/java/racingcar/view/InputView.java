fbb0c704f4173e374fb8c4ab634d45c0da73e6f6
김현빈 <96505736+khv9786@users.noreply.github.com>
2023-10-28 18:16:14 +0900

    feat : View 작성

--- java-racingcar-6/src/main/java/racingcar/view/InputView.java
+++ java-racingcar-6/src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,17 @@
-package src.main.java.racingcar.view;
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Message.NoticeMessage;
 
 public class InputView {
+
+    public static String inputCarsName() {
+        System.out.println(NoticeMessage.Start_Notice);
+        return Console.readLine();
+    }
+
+    public static String inputTrycnt() {
+        System.out.println(NoticeMessage.Cnt_Notice);
+        return Console.readLine();
+    }
 }
--- java-racingcar-6/src/main/java/racingcar/view/OutputView.java
+++ java-racingcar-6/src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,20 @@
-package src.main.java.racingcar.view;
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Message.NoticeMessage;
 
 public class OutputView {
+    public static String printResult() {
+        System.out.println(NoticeMessage.Result_Notice);
+        return Console.readLine();
+    }
+
+    public static String printWinner() {
+        System.out.println(NoticeMessage.Winner_Notice);
+        return Console.readLine();
+    }
+
+    public static void printException(Exception exception) {
+        System.out.println(exception.getMessage());
+    }
 }
